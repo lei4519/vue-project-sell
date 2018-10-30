65064f7c094e87f1230249bccef22652283d6651
lei4519 <939690017@qq.com>
2018-10-30 13:52:55 +0800

    小球完善

--- src/components/shopCart/shopCart.vue
+++ src/components/shopCart/shopCart.vue
@@ -4,7 +4,7 @@
       <div class="content-left" @click="isListShow=!isListShow">
         <div class="logo-wrapper">
           <div class="logo" :class="{'highlight': productInfo.count}">
-            <i class="icon-shopping_cart"></i>
+            <i class="icon-shopping_cart shopping_cart-hook"></i>
           </div>
           <div class="num" v-show="productInfo.count">{{ productInfo.count }}</div>
         </div>
@@ -15,16 +15,6 @@
         <div class="pay" :class="[ text === '去结算' ? 'enough' : 'not-enough']" v-text="text"></div>
       </div>
     </div>
-		<div class="ball-container">
-			<transition-group name="ball"
-			 	@before-enter="beforeEnter"
-				@enter="enter"
-				@after-enter="afterEnter">
-				<div class="ball" v-for="(ball, i) in balls" :key="i" v-show="ball.show">
-					<div class="inner" ></div>
-				</div>
-			</transition-group>
-		</div>
     <transition>
       <div class="shop-cart-list" v-show="isListShow" ref="shop-cart-list">
         <div class="list-header"><h1 class="title">购物车</h1> <span class="empty">清空</span></div>
@@ -48,62 +38,10 @@
 import cartControl from '@/components/cartControl/cartControl.vue'
 
 export default {
-	props: ['cartAddEvent'],
 	data: () => ({
 		seller: [],
-		isListShow: false,
-		balls: [
-			{ show: false },
-			{ show: false },
-			{ show: false },
-			{ show: false },
-			{ show: false }
-		],
-		dropBalls: []
+		isListShow: false
 	}),
-	methods: {
-		beforeEnter(el) {
-			let count = this.balls.length
-			while (count--) {
-				const ball = this.balls[count]
-				if (ball.show) {
-					const rect = ball.el.getBoundingClientRect()
-					const x = rect.left - 32
-					const y = -(rect.top - 22)
-					el.style.display = ''
-					el.style.transform = `translate3d(0, ${y}px, 0)`
-					el.children[0].style.transform = `translate3d(${x}px, 0, 0)`
-				}
-			}
-		},
-		enter(el, done) {
-			el.offsetHeight
-			this.$nextTick(() => {
-				el.style.transform = 'translate3d(0, 0, 0)'
-				el.children[0].style.transform = 'translate3d(0, 0, 0)'
-			})
-		},
-		afterEnter(el) {
-			const ball = this.dropBalls.shift()
-			if (ball) {
-				ball.show = false
-			}
-		}
-	},
-	watch: {
-		cartAddEvent() {
-			const target = this.cartAddEvent.target
-			for (let i = 0; i < this.balls.length; i++) {
-				const ball = this.balls[i]
-				if (!ball.show) {
-					ball.show = true
-					ball.el = target
-					this.dropBalls.push(ball)
-					return
-				}
-			}
-		}
-	},
 	computed: {
 		productInfo() {
 			return this.$store.getters.productInfo
@@ -250,22 +188,6 @@ export default {
 					background-color: #00b43c;
 					color: #fff;
 				}
-			}
-		}
-	}
-	.ball-container {
-		.ball {
-			position: fixed;
-			left: 32px;
-			bottom: 22px;
-			z-index: 99;
-			transition: all 0.4s cubic-bezier(0.49, -0.29, 0.75, 0.41);
-			.inner {
-				width: 16px;
-				height: 16px;
-				border-radius: 50%;
-				background-color: rgb(0, 160, 220);
-				transition: all 0.4s linear;
 			}
 		}
 	}
--- src/views/goods/goods.vue
+++ src/views/goods/goods.vue
@@ -29,7 +29,7 @@
                   <del class="old" v-if="food.oldPrice">¥{{food.oldPrice}}</del>
                 </div>
                 <div class="cartcontrol-wrapper">
-                  <cart-control :lv1="lv1Index" :lv2="lv2Index" :productNum="food.productNum" @cartAdd="cartAdd"></cart-control>
+                  <cart-control :lv1="lv1Index" :lv2="lv2Index" :productNum="food.productNum"></cart-control>
                 </div>
               </div>
             </li>
@@ -37,7 +37,7 @@
         </li>
       </ul>
     </div>
-    <shop-cart :cartAddEvent="cartAddEvent"></shop-cart>
+    <shop-cart ref="shopCart"></shop-cart>
   </div>
 </template>
 
@@ -75,11 +75,6 @@ export default {
 				return
 			}
 			this.foodsScroll.scrollToElement(this.$refs.goodsInfo.children[i], 300)
-		},
-		cartAdd(e) {
-			this.$nextTick(() => {
-				this.cartAddEvent = e
-			})
 		}
 	},
 	computed: {
