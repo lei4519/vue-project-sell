09b58f56a9aeb13c9a03220ae07bff684ccb4fcb
lei4519 <939690017@qq.com>
2019-02-20 14:43:21 +0800

    调整为下载即可运行

--- README.md
+++ README.md
@@ -1,16 +1,23 @@
 # vue-sell
 
 ```
-// 运行
-npm i
+//运行
+git clone git@github.com:lei4519/vue-project-sell.git
 npm start
-// 之后访问http://localhost:3000 即可查看
+
+// 之后通过http://localhost:3000 或本机IP:3000 即可访问
 ```
 
 ## 项目介绍
   - 使用vue+webpack+es6实现饿了么的在线外卖系统
 
-## 技术：
+  ![home](C:\Users\Administrator\Desktop\vue-project-sell\home.png)
+
+    
+
+    
+
+## 技术栈：
   - es6 + eslint书写代码，提高开发效率和代码质量
   - 使用媒体查询配合缩放处理移动端1px 边框实现
   - 使用flex弹性布局
--- prod.server.js
+++ prod.server.js
@@ -2,7 +2,8 @@ const express = require('express')
 const appRoutes = express.Router()
 const data = require('./data.json')
 const html = require('fs').readFileSync('./dist/index.html', 'utf-8')
-var history = require('connect-history-api-fallback')
+const history = require('connect-history-api-fallback')
+const os = require('os')
 
 const app = express()
 
@@ -32,7 +33,21 @@ app.use(express.static('./dist', {
 }))
 app.use(appRoutes)
 
+///////////////////获取本机ip///////////////////////
+function getIPAdress() {
+    var interfaces = os.networkInterfaces();
+    for (var devName in interfaces) {
+        var iface = interfaces[devName];
+        for (var i = 0; i < iface.length; i++) {
+            var alias = iface[i];
+            if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+                return alias.address;
+            }
+        }
+    }
+}
+const myHost = getIPAdress();
 app.listen(3000, (err) => {
   if (err) console.log(err)
-  console.log('Listening at http://localhost:3000')
+  console.log(`Your application is running here : http://127.0.0.1:3000 and http://${myHost}:3000`)
 })
